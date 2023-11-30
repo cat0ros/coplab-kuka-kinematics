43e3e6c3dff651de08a21da10a4200b40a8f0a74
catnull_ros <ros0dev@yandex.ru>
2023-11-30 20:52:26 +0300

    Solved forward kinematics

--- robot_control/scripts/forward_kinematics.py
+++ robot_control/scripts/forward_kinematics.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-links_length = [
-    
-]
-
 def rotate_z(angle):
     return np.array([
         [np.cos(angle), -np.sin(angle), 0, 0],
@@ -39,4 +35,18 @@ def translate(x, y, z):
 def dh(q, alpha, r, d):
     return rotate_z(q) * translate(0, 0, d) * rotate_x(alpha) * translate(r, 0, 0)
 
-def forward_kinematics():
+def forward_kinematics(links_length, q_list):
+    t1 = dh(q_list[0], np.pi / 2, links_length[1], links_length[0])
+    t2 = dh(q_list[1] + np.pi / 2, 0, links_length[2], 0)
+    t3 = dh(q_list[2], np.pi / 2, 0, 0)
+    t4 = dh(q_list[3], 0, 0, links_length[3] + links_length[4])
+    t5 = dh(q_list[4], np.pi / 2, 0, 0)
+    t6 = dh(q_list[5] + np.pi / 2, 0, 0, links_length[5] + links_length[6])
+    
+    pos_vector = t6[3, 0], t6[3, 1], t6[3, 2]
+    phi_x = np.arctan2(t6[2, 0], t6[2, 1])
+    phi_y = np.arctan2(np.sqrt(1 - t6[2, 2]))
+    phi_z = np.arctan2(t6[0, 2], t6[1, 2])
+    orientation = phi_x, phi_y, phi_z
+    
+    return (pos_vector, orientation)
