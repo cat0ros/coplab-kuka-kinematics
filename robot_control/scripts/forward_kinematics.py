abd97642a5006b9873b29c434878fc44367350dc
catnull_ros <ros0dev@yandex.ru>
2023-12-03 01:48:26 +0300

    repair forward kinematics

--- robot_control/scripts/forward_kinematics.py
+++ robot_control/scripts/forward_kinematics.py
@@ -1,52 +1,57 @@
 import numpy as np
 
-def rotate_z(angle):
+def rz(a):
     return np.array([
-        [np.cos(angle), -np.sin(angle), 0, 0],
-        [-np.sin(angle), np.cos(angle), 0, 0],
+        [np.cos(a), -np.sin(a), 0, 0],
+        [np.sin(a), np.cos(a), 0, 0],
         [0, 0, 1, 0],
         [0, 0, 0, 1]
     ])
 
-def rotate_y(angle):
+def ry(a):
     return np.array([
-        [np.cos(angle), 0, np.sin(angle), 0],
-        [0, 0, 1, 0],
-        [-np.sin(angle), 0, np.cos(angle), 0],
+        [np.cos(a), 0, np.sin(a), 0],
+        [0, 1, 0, 0],
+        [-np.sin(a), 0, np.cos(a), 0],
         [0, 0, 0, 1]
     ])
 
-def rotate_x(angle):
+def rx(a):
     return np.array([
-        [0, np.cos(angle), -np.sin(angle), 0],
-        [0, 0, 1, 0],
-        [0, np.sin(angle), np.cos(angle), 0],
+        [1, 0, 0, 0],
+        [0, np.cos(a), -np.sin(a), 0],
+        [0, np.sin(a), np.cos(a), 0],
         [0, 0, 0, 1]
     ])
 
-def translate(x, y, z):
+def trs(x, y, z):
     return np.array([
-        [0, 0, 0, x],
-        [0, 0, 0, y],
-        [0, 0, 0, z],
+        [1, 0, 0, x],
+        [0, 1, 0, y],
+        [0, 0, 1, z],
         [0, 0, 0, 1]
     ])
 
 def dh(q, alpha, r, d):
-    return rotate_z(q) * translate(0, 0, d) * rotate_x(alpha) * translate(r, 0, 0)
+    mul1 = rz(q)
+    mul2 = trs(0, 0, d)
+    mul3 = rx(alpha)
+    mul4 = trs(r, 0, 0)
+    return ((mul1.dot(mul2)).dot(mul3)).dot(mul4)
 
 def forward_kinematics(links_length, q_list):
-    t1 = dh(q_list[0], np.pi / 2, links_length[1], links_length[0])
-    t2 = dh(q_list[1] + np.pi / 2, 0, links_length[2], 0)
-    t3 = dh(q_list[2], np.pi / 2, 0, 0)
-    t4 = dh(q_list[3], 0, 0, links_length[3] + links_length[4])
+    t1 = dh(q_list[0], -np.pi / 2, links_length[1], links_length[0])
+    t2 = dh(q_list[1], 0, links_length[2], 0)
+    t3 = dh(q_list[2] - np.pi / 2, np.pi / 2, 0, 0)
+    t4 = dh(q_list[3], -np.pi / 2, 0, -links_length[3])
     t5 = dh(q_list[4], np.pi / 2, 0, 0)
-    t6 = dh(q_list[5] + np.pi / 2, 0, 0, links_length[5] + links_length[6])
+    t6 = dh(q_list[5], np.pi / 2, 0, -links_length[4])
+    fk_sol = ((((t1.dot(t2)).dot(t3)).dot(t4)).dot(t5)).dot(t6)
+    pos_vector = fk_sol[0, 3], fk_sol[1, 3], fk_sol[2, 3]
+    eulerY = np.arctan2(np.sqrt(fk_sol[0, 2]**2 + fk_sol[1, 2]**2), fk_sol[2, 2])
+    eulerZ = np.arctan2(fk_sol[2, 1], -fk_sol[2, 0])
+    eulerX = np.arctan2(fk_sol[1, 2], fk_sol[0, 2])
     
-    pos_vector = t6[3, 0], t6[3, 1], t6[3, 2]
-    phi_x = np.arctan2(t6[2, 0], t6[2, 1])
-    phi_y = np.arctan2(np.sqrt(1 - t6[2, 2]))
-    phi_z = np.arctan2(t6[0, 2], t6[1, 2])
-    orientation = phi_x, phi_y, phi_z
+    orientation = eulerX, eulerY, eulerZ
     
     return (pos_vector, orientation)
--- robot_control/scripts/robot_run.py
+++ robot_control/scripts/robot_run.py
@@ -1,14 +1,48 @@
 from . import sim
 from . import build_circle
+from . import forward_kinematics
 
 import numpy as np
 import sys
+import time
 
+class RobotControl:
+    def __init__(self, clientID):
+        err1, self.j1 = sim.simxGetObjectHandle(clientID, 'joint_a1', sim.simx_opmode_oneshot_wait)
+        err2, self.j2 = sim.simxGetObjectHandle(clientID, 'joint_a2', sim.simx_opmode_oneshot_wait)
+        err3, self.j3 = sim.simxGetObjectHandle(clientID, 'joint_a3', sim.simx_opmode_oneshot_wait)
+        err4, self.j4 = sim.simxGetObjectHandle(clientID, 'joint_a4', sim.simx_opmode_oneshot_wait)
+        err5, self.j5 = sim.simxGetObjectHandle(clientID, 'joint_a5', sim.simx_opmode_oneshot_wait)
+        err5, self.j6 = sim.simxGetObjectHandle(clientID, 'joint_a6', sim.simx_opmode_oneshot_wait)
+        print(err1)
+        self.currentGeneralized = np.array([0, 0, 0, 0, 0, 0], float)
+        self.set_generalized(clientID, self.currentGeneralized)
+
+    def get_generalized(self, clientID):
+        err1, self.currentGeneralized[0] = sim.simxGetJointPosition(clientID, self.j1, sim.simx_opmode_oneshot_wait)
+        err2, self.currentGeneralized[1] = sim.simxGetJointPosition(clientID, self.j2, sim.simx_opmode_oneshot_wait)
+        err3, self.currentGeneralized[2] = sim.simxGetJointPosition(clientID, self.j3, sim.simx_opmode_oneshot_wait)
+        err4, self.currentGeneralized[3] = sim.simxGetJointPosition(clientID, self.j4, sim.simx_opmode_oneshot_wait)
+        err5, self.currentGeneralized[4] = sim.simxGetJointPosition(clientID, self.j5, sim.simx_opmode_oneshot_wait)
+        err6, self.currentGeneralized[5] = sim.simxGetJointPosition(clientID, self.j6, sim.simx_opmode_oneshot_wait)
+        print(err1)
+        
+    def set_generalized(self, clientID, generalized):
+        generalized = np.deg2rad(generalized)
+        sim.simxSetJointTargetPosition(clientID, self.j1, generalized[0], sim.simx_opmode_oneshot_wait)
+        sim.simxSetJointTargetPosition(clientID, self.j2, generalized[1], sim.simx_opmode_oneshot_wait)
+        sim.simxSetJointTargetPosition(clientID, self.j3, generalized[2], sim.simx_opmode_oneshot_wait)
+        sim.simxSetJointTargetPosition(clientID, self.j4, generalized[3], sim.simx_opmode_oneshot_wait)
+        sim.simxSetJointTargetPosition(clientID, self.j5, generalized[4], sim.simx_opmode_oneshot_wait)
+        sim.simxSetJointTargetPosition(clientID, self.j6, generalized[5], sim.simx_opmode_oneshot_wait)
+        self.currentGeneralized = generalized
+    
 def connect_coppelia(ip, port):
     sim.simxFinish(-1)
     clientId = sim.simxStart(ip, port, True, True, 5000, 5)
     if clientId != -1:
         print("Connected to Remote API Server")
+        return clientId
     else:
         print("Connection failed")
         sys.exit('Could not reconnect')
@@ -21,5 +55,18 @@ def main():
     
 
 if __name__ == '__main__':
-    connect_coppelia('127.0.0.1', 19990)
+    clientID = connect_coppelia('127.0.0.1', 19990)
+    links_length = [0.675, 0.260, 0.680, 0.670, 0.158]
     main()
+    robot = RobotControl(clientID)
+    robot.set_generalized(clientID, np.array([90, -90, 90, 30, 30, 0], float))
+    time.sleep(3)
+    robot.get_generalized(clientID)
+    pos, rot = forward_kinematics.forward_kinematics(links_length, robot.currentGeneralized)
+    x, y, z = pos[0], pos[1], pos[2]
+    err1, cuboid = sim.simxGetObjectHandle(clientID, 'Cuboid', sim.simx_opmode_oneshot_wait)
+    err1, robot_handle = sim.simxGetObjectHandle(clientID, 'base_link_respondable', sim.simx_opmode_oneshot_wait)
+    sim.simxSetObjectPosition(clientID, cuboid, robot_handle, [x, y, z], sim.simx_opmode_oneshot_wait)
+    sim.simxSetObjectOrientation(clientID, cuboid, robot_handle, rot, sim.simx_opmode_oneshot_wait)
+    err2, orient2 = sim.simxGetObjectOrientation(clientID, cuboid, robot_handle, sim.simx_opmode_oneshot_wait)
+    print(err1, err2)
