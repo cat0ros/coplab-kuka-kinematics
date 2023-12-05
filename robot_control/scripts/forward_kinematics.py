fda9f703654f372560a18ca75b8b249a46404255
catnull_ros <ros0dev@yandex.ru>
2023-12-05 15:14:10 +0300

    add inverse kinematics algorithm

--- robot_control/scripts/forward_kinematics.py
+++ robot_control/scripts/forward_kinematics.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 def rz(a):
     return np.array([
@@ -42,16 +43,16 @@ def dh(q, alpha, r, d):
 def forward_kinematics(links_length, q_list):
     t1 = dh(q_list[0], -np.pi / 2, links_length[1], links_length[0])
     t2 = dh(q_list[1], 0, links_length[2], 0)
-    t3 = dh(q_list[2] - np.pi / 2, np.pi / 2, 0, 0)
-    t4 = dh(q_list[3], -np.pi / 2, 0, -links_length[3])
+    t3 = dh(q_list[2] - (np.pi / 2), np.pi / 2, 0, 0)
+    t4 = dh(q_list[3], -np.pi / 2, -0.035, -links_length[3])
     t5 = dh(q_list[4], np.pi / 2, 0, 0)
     t6 = dh(q_list[5], np.pi / 2, 0, -links_length[4])
     fk_sol = ((((t1.dot(t2)).dot(t3)).dot(t4)).dot(t5)).dot(t6)
     pos_vector = fk_sol[0, 3], fk_sol[1, 3], fk_sol[2, 3]
+    
     eulerY = np.arctan2(np.sqrt(fk_sol[0, 2]**2 + fk_sol[1, 2]**2), fk_sol[2, 2])
     eulerZ = np.arctan2(fk_sol[2, 1], -fk_sol[2, 0])
     eulerX = np.arctan2(fk_sol[1, 2], fk_sol[0, 2])
     
     orientation = eulerX, eulerY, eulerZ
-    
-    return (pos_vector, orientation)
+    return np.array([pos_vector, orientation])
--- robot_control/scripts/robot_run.py
+++ robot_control/scripts/robot_run.py
@@ -1,6 +1,7 @@
 from . import sim
 from . import build_circle
 from . import forward_kinematics
+from . import ik_solver
 
 import numpy as np
 import sys
@@ -14,7 +15,6 @@ class RobotControl:
         err4, self.j4 = sim.simxGetObjectHandle(clientID, 'joint_a4', sim.simx_opmode_oneshot_wait)
         err5, self.j5 = sim.simxGetObjectHandle(clientID, 'joint_a5', sim.simx_opmode_oneshot_wait)
         err5, self.j6 = sim.simxGetObjectHandle(clientID, 'joint_a6', sim.simx_opmode_oneshot_wait)
-        print(err1)
         self.currentGeneralized = np.array([0, 0, 0, 0, 0, 0], float)
         self.set_generalized(clientID, self.currentGeneralized)
 
@@ -25,7 +25,6 @@ class RobotControl:
         err4, self.currentGeneralized[3] = sim.simxGetJointPosition(clientID, self.j4, sim.simx_opmode_oneshot_wait)
         err5, self.currentGeneralized[4] = sim.simxGetJointPosition(clientID, self.j5, sim.simx_opmode_oneshot_wait)
         err6, self.currentGeneralized[5] = sim.simxGetJointPosition(clientID, self.j6, sim.simx_opmode_oneshot_wait)
-        print(err1)
         
     def set_generalized(self, clientID, generalized):
         generalized = np.deg2rad(generalized)
@@ -48,9 +47,9 @@ def connect_coppelia(ip, port):
         sys.exit('Could not reconnect')
 
 def main():
-    point1 = np.array([200, 100, 250])
-    point2 = np.array([250, 100, 300])
-    point3 = np.array([150, 100, 100])
+    point1 = np.array([0.200, 100, 0.250])
+    point2 = np.array([0.250, 100, 0.300])
+    point3 = np.array([0.150, 0.100, 0.100])
     x, z, radius = build_circle.build(point1, point2, point3)
     
 
@@ -59,14 +58,41 @@ if __name__ == '__main__':
     links_length = [0.675, 0.260, 0.680, 0.670, 0.158]
     main()
     robot = RobotControl(clientID)
-    robot.set_generalized(clientID, np.array([90, -90, 90, 30, 30, 0], float))
-    time.sleep(3)
-    robot.get_generalized(clientID)
-    pos, rot = forward_kinematics.forward_kinematics(links_length, robot.currentGeneralized)
+    gens = np.deg2rad(np.array([90, -90, 45, 0, 0, 0]))
+    pos, rot = forward_kinematics.forward_kinematics(links_length, gens)
+    robot.set_generalized(clientID, np.rad2deg(gens))
     x, y, z = pos[0], pos[1], pos[2]
-    err1, cuboid = sim.simxGetObjectHandle(clientID, 'Cuboid', sim.simx_opmode_oneshot_wait)
-    err1, robot_handle = sim.simxGetObjectHandle(clientID, 'base_link_respondable', sim.simx_opmode_oneshot_wait)
-    sim.simxSetObjectPosition(clientID, cuboid, robot_handle, [x, y, z], sim.simx_opmode_oneshot_wait)
-    sim.simxSetObjectOrientation(clientID, cuboid, robot_handle, rot, sim.simx_opmode_oneshot_wait)
-    err2, orient2 = sim.simxGetObjectOrientation(clientID, cuboid, robot_handle, sim.simx_opmode_oneshot_wait)
-    print(err1, err2)
+    #q = ik_solver.numerical_inverse_kinematics(pos, links_length, gens)
+    #pos[2] -= 0.2
+    #q = ik_solver.numerical_inverse_kinematics(pos, links_length, gens)
+    #print(np.rad2deg(q))
+    p_desired = np.array([x, y, z])  # Desired position
+    orientation_desired = np.array([0, 0, 0])
+    q_solution = ik_solver.ik_sol(links_length, p_desired, orientation_desired, np.deg2rad(np.array([0, 0, 0, 0, 0, 0])))
+    fk_result = forward_kinematics.forward_kinematics(links_length, q_solution)
+    print(np.round(fk_result[0], 2))
+    print(np.round((x, y, z), 2))
+    q_new = np.array([0, 0, 0, 0, 0, 0])
+    for i in range(len(q_solution)):
+        q_new[i] = np.rad2deg(np.arctan2(np.sin(q_solution[i]), np.cos(q_solution[i])))
+
+    gens = np.array([0, 0, 0, 0, 0, 0])
+    robot.set_generalized(clientID, np.rad2deg(gens))
+    time.sleep(3)
+    robot.set_generalized(clientID, q_new)
+    err, cuboid = sim.simxGetObjectHandle(clientID, 'Cuboid', sim.simx_opmode_oneshot_wait)
+    err, robot_handle = sim.simxGetObjectHandle(clientID, 'base_link_respondable', sim.simx_opmode_oneshot_wait)
+    err2, cub_pos = sim.simxGetObjectPosition(clientID, cuboid, robot_handle, sim.simx_opmode_oneshot_wait)
+    err3, cub_or = sim.simxGetObjectPosition(clientID, cuboid, robot_handle, sim.simx_opmode_oneshot_wait)
+    theta1 = np.rad2deg(np.arctan2(cub_pos[0], cub_pos[1]))
+    q_solution = ik_solver.ik_sol(links_length, cub_pos, cub_or, np.deg2rad(np.array([0, 0, 0, 0, 0, 0])))
+    q_new = np.array([0, 0, 0, 0, 0, 0])
+    for i in range(len(q_solution)):
+        angle = np.rad2deg(q_solution[i])
+        q_new[i] = (((angle + 180) % 360) + 360) % 360 - 180
+    robot.set_generalized(clientID, q_new)
+    print('debug-------------------------------------------------------------------')
+    fk_result = forward_kinematics.forward_kinematics(links_length, np.deg2rad(q_new))
+    print(np.round(fk_result, 3))
+    print(np.round(cub_pos, 3))
+    print(np.rad2deg(fk_result[1]))
